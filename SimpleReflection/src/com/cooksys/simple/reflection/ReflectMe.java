3a7376280ffef3d881aa5f29213de8f43b8e5e39
cooksystems1 <cooksystems1@mdi-16-PC>
2014-09-04 09:33:49 -0400

    Making my change

--- SimpleReflection/src/com/cooksys/simple/reflection/ReflectMe.java
+++ SimpleReflection/src/com/cooksys/simple/reflection/ReflectMe.java
@@ -2,6 +2,8 @@ package com.cooksys.simple.reflection;
 
 public class ReflectMe {
 	
+	
+	//Test commit
 	@MyAnnotation("A String in my Annotation!")
 	public String anAnnotatedMethod(String string){
 		return string;
