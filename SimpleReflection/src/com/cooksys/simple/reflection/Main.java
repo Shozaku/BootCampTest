90b9102f7f8b2de691f5c9005e47d4c7d260180b
tchou84 <tchou84@yahoo.com>
2014-09-04 09:56:06 -0400

    Added Cool

--- SimpleReflection/src/com/cooksys/simple/reflection/Main.java
+++ SimpleReflection/src/com/cooksys/simple/reflection/Main.java
@@ -11,6 +11,11 @@ public class Main {
 	public static void main(String[] args) {
 		Main main = new Main();
 		main.doThings();
+		main.doCoolThings();
+	}
+	
+	private void doCoolThings() {
+		System.out.println(cool);
 	}
 	
 	private void doThings(){
